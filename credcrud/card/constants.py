8d050035834f60db23d0555c051abe18c458955f
Jonhnatha Trigueiro <joepreludian@gmail.com>
2023-11-24 21:03:37 -0300

    update constants - AMEX cards has 15 digits, not 16. add this edge case

--- credcrud/card/constants.py
+++ credcrud/card/constants.py
@@ -5,7 +5,8 @@ class CardConstants(IntEnum):
     HOLDER_MIN_SIZE = 2
     HOLDER_MAX_SIZE = 128
 
-    CARD_NUMBER_SIZE = 16
+    CARD_NUMBER_MIN_SIZE = 15
+    CARD_NUMBER_MAX_SIZE = 16
 
     CVV_MIN_SIZE = 3
     CVV_MAX_SIZE = 4
--- credcrud/card/models.py
+++ credcrud/card/models.py
@@ -9,7 +9,7 @@ class Card(Base):
     __tablename__ = "cards"
 
     id = Column(UUID(as_uuid=True), unique=True, primary_key=True, index=True)
-    card_number = Column(String(CardConstants.CARD_NUMBER_SIZE.value), unique=True, nullable=False)
+    card_number = Column(String(CardConstants.CARD_NUMBER_MAX_SIZE.value), unique=True, nullable=False)
     card_holder = Column(String(CardConstants.HOLDER_MAX_SIZE.value), nullable=False)
     expiration_date = Column(Date, nullable=False)
     cvv = Column(String(CardConstants.CVV_MAX_SIZE.value), nullable=True)
