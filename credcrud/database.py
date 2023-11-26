b975eba3f7df8508a4c5ff2bdb93a2acaebcf898
Jonhnatha Trigueiro <joepreludian@gmail.com>
2023-11-26 19:02:22 -0300

    refactor database in order to remove deprecation message

--- credcrud/database.py
+++ credcrud/database.py
@@ -2,8 +2,7 @@ import os
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_CREDENTIALS = os.environ.get("DATABASE_CREDENTIALS")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
